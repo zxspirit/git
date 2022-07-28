f2c9ed496cb67b978e956cd073330b2ee294a0e3
zx <1032765602@qq.com>
2022-07-28 22:48:12 +0800

    ee

---
+++ src/main/java/com/example/zxgit/ZxgitApplication.java
@@ -0,0 +1,13 @@
+package com.example.zxgit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZxgitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZxgitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/zxgit/ZxgitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zxgit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZxgitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
